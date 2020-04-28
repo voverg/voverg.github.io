336ca4092bae5d1bad19ca4ba28e51e8d1f0a095
voverg <voverg81@mail.ru>
2020-04-28 17:13:31 +0300

    Created about me section

--- js/burger.js
+++ js/burger.js
@@ -1,26 +1,38 @@
 // Select Html elements
+// Main nav elements
 const body = document.querySelector('body');
 const mainNav = document.querySelector('.main-nav');
+// Burger menu elements
 const burgerMenu = document.querySelector('.burger-menu');
 const button = burgerMenu.querySelector('.burger-menu__button');
 const burgerNav = burgerMenu.querySelector('.burger-menu__nav');
 const overlay = burgerMenu.querySelector('.burger-menu__overlay');
+// Progress bars elements
+const circles = document.querySelectorAll('.progress__circle');
+const radius = circles[0].r.baseVal.value;
+const circumference = 2 * Math.PI * radius;
+const persentsEl = document.querySelectorAll('.skill__persents span');
 
-function toggleMenu() {
-    burgerMenu.classList.toggle('burger-menu_active');
-    body.classList.toggle('hidden');
+// Progress bar
+circles.forEach((elem, index) => {
+    const persent = elem.dataset.persent;
+    setProgress(persent, index);
+})
+
+function setProgress(persent, index) {
+    let current = 0;
+    const progress = circumference / 100 * persent;
+    let intervalId = setInterval(() => {
+        if (current > progress) {
+            clearInterval(intervalId);
+        } else {
+            circles[index].style.strokeDashoffset = circumference - current;
+            persentsEl[index].textContent =  Math.round(current / 2.51);
+        }
+        current++;
+    }, 1)
 }
 
-function smoothScroll(event, link) {
-    event.preventDefault();
-    const href = link.getAttribute('href') || link.dataset.href;
-    document.querySelector('' + href).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    }) 
-}
-
-// ---------------------- Event listeners
 // Main nav
 mainNav.addEventListener('click', event => {
     link = event.target;
@@ -29,6 +41,11 @@ mainNav.addEventListener('click', event => {
     }
 })
 // Burger menu
+function toggleMenu() {
+    burgerMenu.classList.toggle('burger-menu_active');
+    body.classList.toggle('hidden');
+}
+
 button.addEventListener('click', event => {
     event.preventDefault();
     toggleMenu();
@@ -45,6 +62,15 @@ burgerNav.addEventListener('click', event => {
 overlay.addEventListener('click', toggleMenu);
 
 // Show fixed nav
+function smoothScroll(event, link) {
+    event.preventDefault();
+    const href = link.getAttribute('href') || link.dataset.href;
+    document.querySelector('' + href).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    }) 
+}
+
 window.addEventListener('scroll', function() {
     if(window.pageYOffset > 200){
         mainNav.classList.add('main-nav__fixed');
