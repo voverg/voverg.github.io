ddc09682d5ff9c1ca979b73b1b0f86e555f5e4d9
voverg <voverg81@mail.ru>
2021-06-24 14:33:50 +0300

    Add into nav a smooth scroll to a target element

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Select Html elements
 const body = document.querySelector('body');
+const header = document.querySelector('header');
 // Burger menu elements
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerBtn = burgerMenu.querySelector('.burger-menu__button');
@@ -7,20 +8,13 @@ const burgerNav = burgerMenu.querySelector('.burger-menu__nav');
 const overlay = burgerMenu.querySelector('.burger-menu__overlay');
 
 
-
-
 // Burger menu
-function toggleMenu() {
-    burgerMenu.classList.toggle('burger-menu_active');
-    body.classList.toggle('hidden');
-}
-
-burgerBtn.addEventListener('click', event => {
+burgerBtn.addEventListener('click', (event) => {
     event.preventDefault();
     toggleMenu();
 });
 
-burgerNav.addEventListener('click', event => {
+burgerNav.addEventListener('click', (event) => {
     const link = event.target;
     if (link.classList.contains('nav-item-js')) {
         smoothScroll(event, link);
@@ -30,12 +24,24 @@ burgerNav.addEventListener('click', event => {
 
 overlay.addEventListener('click', toggleMenu);
 
-// Show fixed nav
+
+function toggleMenu() {
+    burgerMenu.classList.toggle('burger-menu_active');
+    body.classList.toggle('hidden');
+}
+
 function smoothScroll(event, link) {
     event.preventDefault();
     const href = link.getAttribute('href') || link.dataset.href;
-    document.querySelector('' + href).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    })
+    const target = document.querySelector(`${href}`);
+
+    if (target !== null) {
+      const headerHeight = burgerBtn.clientHeight || header.clientHeight;
+      const topPosition = target.offsetTop - headerHeight;
+
+      window.scrollTo({
+        top: topPosition,
+        behavior: 'smooth'
+      });
+    }
 }
